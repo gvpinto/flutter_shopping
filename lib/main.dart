585096ca4e12421458f785a46885232f07e213a8
Glenn Pinto <gvpinto@gmail.com>
2021-10-03 12:29:48 -0400

    initial commit - successfully connected to Firebase datastore

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Firebase"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () =>
+              FirebaseFirestore.instance.collection('testing').add({
+            'timestamp': Timestamp.fromDate(
+              DateTime.now(),
+            ),
+          }),
+          child: const Icon(Icons.add),
+        ),
+        body: StreamBuilder(
+            stream:
+                FirebaseFirestore.instance.collection('testing').snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError) {
+                return const Text('Something went wrong');
+              }
+
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Text("Loading");
+              }
+
+              return ListView(
+                children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                  Map<String, dynamic> data =
+                      document.data()! as Map<String, dynamic>;
+                  return ListTile(
+                    title: Text(data['timestamp'].toDate().toString()),
+                  );
+                }).toList(),
+              );
+            }),
+      ),
+    );
+  }
+}
